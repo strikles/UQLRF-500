647432c7d583645d6ae02187078495c6de775f2b
strikles <strikles@gmail.com>
2021-04-25 06:02:31 +0000

    fix prev

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,17 @@
-extern crate rand;
+#[macro_use]
 extern crate vst;
+extern crate rand;
 
 use rand::Rng;
 use std::fmt;
+
 use vst::buffer::AudioBuffer;
-use vst::plugin::{Plugin, Info};
+use vst::plugin::{Info, Plugin, PluginParameters};
 
 pub struct Karplus {
     frequency: f32,
     sample_rate: u32,
     buffer: Vec<f32>
-}
-
-impl Karplus {
-    pub fn new(frequency: f32, sample_rate: u32) -> Karplus {
-        let size = (sample_rate as f32 / frequency) as usize;
-        let mut v: Vec<f32> = Vec::with_capacity(size);
-        let mut r = rand::thread_rng();
-        for _ in 0..size {
-            v.push((r.gen::<f32>() * 2.0) -1.0);
-        }
-        Karplus{
-            frequency: frequency,
-            sample_rate: sample_rate,
-            buffer: v
-        }
-    }
-
-    pub fn sample(&mut self, damping: f32) -> f32 {
-        let v: f32 = self.buffer.remove(0);
-        let s: f32 = (v + self.buffer[0])*0.5 * damping;
-
-        self.buffer.push(s);
-        s
-    }
 }
 
 impl fmt::Display for Karplus {
@@ -53,6 +31,29 @@ impl Default for Karplus {
 
 
 impl Plugin for Karplus {
+    
+    fn new(frequency: f32, sample_rate: u32) -> Karplus {
+        let size = (sample_rate as f32 / frequency) as usize;
+        let mut v: Vec<f32> = Vec::with_capacity(size);
+        let mut r = rand::thread_rng();
+        for _ in 0..size {
+            v.push((r.gen::<f32>() * 2.0) -1.0);
+        }
+        Karplus{
+            frequency: frequency,
+            sample_rate: sample_rate,
+            buffer: v
+        }
+    }
+
+    fn sample(&mut self, damping: f32) -> f32 {
+        let v: f32 = self.buffer.remove(0);
+        let s: f32 = (v + self.buffer[0])*0.5 * damping;
+
+        self.buffer.push(s);
+        s
+    }
+    
     fn get_info(&self) -> Info {
         Info {
             name: "UQLRF".to_string(),
