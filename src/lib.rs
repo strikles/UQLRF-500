1beeaa73f804f0d428ef0eb9b48453cf01e625f2
strikles <strikles@gmail.com>
2021-04-25 06:40:49 +0000

    whisper

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@
 extern crate vst;
 extern crate rand;
 
-use rand::Rng;
-use std::fmt;
-
+use vst::plugin::{Info, Plugin, Category, CanDo};
 use vst::buffer::AudioBuffer;
-use vst::plugin::{Info, Plugin, PluginParameters};
+use vst::event::Event;
+use vst::api::{Events, Supported};
+use rand::random;
 
-pub struct Karplus {
+#[derive(Default)]
+struct Karplus {
     frequency: f32,
     sample_rate: u32,
     buffer: Vec<f32>
@@ -48,6 +49,82 @@ impl Plugin for Karplus {
         }
     }
     
+    // Here's the function that allows us to receive events
+    fn process_events(&mut self, events: &Events) {
+
+        // Some events aren't MIDI events - so let's do a match
+        // to make sure we only get MIDI, since that's all we care about.
+        for event in events.events() {
+            match event {
+                Event::Midi(ev) => {
+
+                    // Check if it's a noteon or noteoff event.
+                    // This is difficult to explain without knowing how the MIDI standard works.
+                    // Basically, the first byte of data tells us if this signal is a note on event
+                    // or a note off event.  You can read more about that here: 
+                    // https://www.midi.org/specifications/item/table-1-summary-of-midi-message
+                    match ev.data[0] {
+
+                        // if note on, increment our counter
+                        144 => self.notes += 1u8,
+
+                        // if note off, decrement our counter
+                        128 => self.notes -= 1u8,
+                        _ => (),
+                    }
+                    // if we cared about the pitch of the note, it's stored in `ev.data[1]`.
+                },
+                // We don't care if we get any other type of event
+                _ => (),
+            }
+        }
+    }
+
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+
+        // `buffer.split()` gives us a tuple containing the 
+        // input and output buffers.  We only care about the
+        // output, so we can ignore the input by using `_`.
+        let (_, output_buffer) = buffer.split();
+
+        // We only want to process *anything* if a note is being held.
+        // Else, we can fill the output buffer with silence.
+        if self.notes == 0 {
+            for output_channel in output_buffer.into_iter() {
+                // Let's iterate over every sample in our channel.
+                for output_sample in output_channel {
+                    *output_sample = 0.0;
+                }
+            }
+            return;
+        }
+
+        // Now, we want to loop over our output channels.  This
+        // includes our left and right channels (or more, if you
+        // are working with surround sound).
+        for output_channel in output_buffer.into_iter() {
+            // Let's iterate over every sample in our channel.
+            for output_sample in output_channel {
+                // For every sample, we want to generate a random value
+                // from -1.0 to 1.0.
+                *output_sample = (random::<f32>() - 0.5f32) * 2f32;
+            }
+        }
+    }
+
+    // It's good to tell our host what our plugin can do.
+    // Some VST hosts might not send any midi events to our plugin
+    // if we don't explicitly tell them that the plugin can handle them.
+    fn can_do(&self, can_do: CanDo) -> Supported {
+        match can_do {
+            // Tell our host that the plugin supports receiving MIDI messages
+            CanDo::ReceiveMidiEvent => Supported::Yes,
+            // Maybe it also supports ather things
+            _ => Supported::Maybe,
+        }
+    }
+
+    /*
     fn new(frequency: f32, sample_rate: u32) -> Karplus {
         let size = (sample_rate as f32 / frequency) as usize;
         let mut v: Vec<f32> = Vec::with_capacity(size);
@@ -69,6 +146,7 @@ impl Plugin for Karplus {
         self.buffer.push(s);
         s
     }
+    */
 }
 
 impl PluginParameters for Karplus {
@@ -113,6 +191,7 @@ impl PluginParameters for Karplus {
 
 plugin_main!(Karplus);
 
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -133,3 +212,4 @@ mod tests {
         assert_eq!(sample, 0.5)
     }
 }
+*/
