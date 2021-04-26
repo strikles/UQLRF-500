2fcb8cee7b988f576644a5d0448ab3d51e4cbc53
strikles <strikles@gmail.com>
2021-04-26 08:20:11 +0000

    whisper

--- src/lib.rs
+++ src/lib.rs
@@ -22,20 +22,42 @@ struct Note {
     is_released: bool,
 }
 
-#[derive(Default)]
 struct Karplus {
     params: Arc<KarplusParameters>,
     notes: Vec<Note>,
     sample_rate: f32,
     time: f64
+    // Noise functions
+    fn_perlin: Perlin,
+    fn_value: Value,
+    fn_worley: Worley,
+    fn_ridged_multi: RidgedMulti,
+    fn_open_simplex: OpenSimplex,
+    fn_billow: Billow,
+    fn_cylinders: Cylinders,
+    fn_hybrid_multi: HybridMulti,
+    fn_basic_multi: BasicMulti,
 }
 
-#[derive(Default)]
-struct KarplusParameters {
-    gain: AtomicFloat,
-    attack_duration: AtomicFloat,
-    release_duration: AtomicFloat,
-    host: HostCallback
+pub struct KarplusParameters {
+    pub distortion: AtomicFloat,
+    pub attack_duration: AtomicFloat,
+    pub release_duration: AtomicFloat,
+    // Amounts
+    pub a_white_noise: AtomicFloat,
+    pub a_perlin: AtomicFloat,
+    pub a_value: AtomicFloat,
+    pub a_worley: AtomicFloat,
+    pub a_ridged_multi: AtomicFloat,
+    pub a_open_simplex: AtomicFloat,
+    pub a_billow: AtomicFloat,
+    pub a_cylinders: AtomicFloat,
+    pub a_hybrid_multi: AtomicFloat,
+    pub a_basic_multi: AtomicFloat,
+
+    pub attack_duration: AtomicFloat,
+    pub release_duration: AtomicFloat,
+    pub host: HostCallback
 }
 
 pub fn midi_pitch_to_freq(pitch: u8) -> f64 {
@@ -45,6 +67,30 @@ pub fn midi_pitch_to_freq(pitch: u8) -> f64 {
     // Midi notes can be 0-127
     ((f64::from(pitch as i8 - A4_PITCH)) / 12.).exp2() * A4_FREQ
 }
+
+impl Default for Karplus {
+    fn default() -> Karplus {
+        Karplus {
+            notes: vec![],
+            sample_rate: 44100.0,
+            time: 0.0,
+            // Amounts
+            params: Arc::new(KarplusParameters::default()),
+
+            // Noise functions
+            fn_perlin: Perlin::new(),
+            fn_value: Value::new(),
+            fn_worley: Worley::new(),
+            fn_ridged_multi: RidgedMulti::new(),
+            fn_open_simplex: OpenSimplex::new(),
+            fn_billow: Billow::new(),
+            fn_cylinders: Cylinders::new(),
+            fn_hybrid_multi: HybridMulti::new(),
+            fn_basic_multi: BasicMulti::new(),
+        }
+    }
+}
+
 
 impl Plugin for Karplus {
     
@@ -133,58 +179,75 @@ impl Plugin for Karplus {
             // ...and remove finished notes.
             self.notes.retain(|n| n.alpha > 0.0);
 
+
             // Sum up all the different notes and noise types
             if !self.notes.is_empty() {
                 let mut signal = 0.0;
-                let _params = self.params.deref();
+                let params = self.params.deref();
+
 
                 for note in &self.notes {
-                    let _point = [0.0, self.time * midi_pitch_to_freq(note.note)];
+                    let point = [0.0, self.time * midi_pitch_to_freq(note.note)];
 
-                    if note.alpha > 0.0001 {
-                        signal += ((random::<f64>() - 0.5) * 2.0) as f64 * note.alpha;
+                    if params.a_white_noise.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += ((random::<f64>() - 0.5) * 2.0) * params.a_white_noise.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_perlin.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_perlin.get(point) * params.a_perlin.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_value.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_value.get(point) * params.a_value.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_worley.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_worley.get(point) * params.a_worley.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_ridged_multi.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_ridged_multi.get(point) * params.a_ridged_multi.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_open_simplex.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_open_simplex.get(point) * params.a_open_simplex.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_billow.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_billow.get(point) * params.a_billow.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_cylinders.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_cylinders.get(point) * params.a_cylinders.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_hybrid_multi.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_hybrid_multi.get(point) * params.a_hybrid_multi.get() as f64 * note.alpha;
+                    }
+
+                    if params.a_basic_multi.get() > 0.0 && note.alpha > 0.0001 {
+                        signal += self.fn_basic_multi.get(point) * params.a_basic_multi.get() as f64 * note.alpha;
                     }
                 }
 
+
                 output_sample = signal as f32;
                 self.time += per_sample;
-                
             } else {
                 output_sample = 0.0;
             }
 
             for buf_idx in 0..output_count {
                 let buff = outputs.get_mut(buf_idx);
-                buff[sample_idx] = 0.5 * (output_sample + prev_output_sample)
+                buff[sample_idx] = 0.5 * (output_sample + prev_output_sample) * params.distortion.get();
             }
-            
-            prev_output_sample = output_sample;
         }
     }
 
-    /*
-    fn new(frequency: f32, sample_rate: u32) -> Karplus {
-        let size = (sample_rate as f32 / frequency) as usize;
-        let mut v: Vec<f32> = Vec::with_capacity(size);
-        let mut r = rand::thread_rng();
-        for _ in 0..size {
-            v.push((r.gen::<f32>() * 2.0) -1.0);
-        }
-        Karplus{
-            frequency: frequency,
-            sample_rate: sample_rate,
-            buffer: v
-        }
+    fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
+        Arc::clone(&self.params) as Arc<dyn PluginParameters>
     }
-
-    fn process(&mut self, damping: f32) -> f32 {
-        let v: f32 = self.buffer.remove(0);
-        let s: f32 = (v + self.buffer[0])*0.5 * damping;
-
-        self.buffer.push(s);
-        s
-    }
-    */
+}
 
     // It's good to tell our host what our plugin can do.
     // Some VST hosts might not send any midi events to our plugin
@@ -199,66 +262,103 @@ impl Plugin for Karplus {
     }
 }
 
-impl PluginParameters for KarplusParameters {
-    fn get_parameter(&self, index: i32) -> f32 {
-        match index {
-            0 => self.gain.get(),
-            1 => self.attack_duration.get(),
-            2 => self.release_duration.get(),
-            _ => 0.0,
-        }
-    }
 
-    fn set_parameter(&self, index: i32, value: f32) {
-        #[allow(clippy::single_match)]
+impl PluginParameters for KarplusParameters {
+    fn get_parameter_text(&self, index: i32) -> String {
         match index {
-            0 => self.gain.set(value),
-            1 => self.attack_duration.set(value),
-            2 => self.release_duration.set(value),
-            _ => (),
+            0 => format!("{:.1}%", self.a_white_noise.get() * 100.0),
+            1 => format!("{:.1}%", self.a_perlin.get() * 100.0),
+            2 => format!("{:.1}%", self.a_value.get() * 100.0),
+            3 => format!("{:.1}%", self.a_worley.get() * 100.0),
+            4 => format!("{:.1}%", self.a_ridged_multi.get() * 100.0),
+            5 => format!("{:.1}%", self.a_open_simplex.get() * 100.0),
+            6 => format!("{:.1}%", self.a_billow.get() * 100.0),
+            7 => format!("{:.1}%", self.a_cylinders.get() * 100.0),
+            8 => format!("{:.1}%", self.a_hybrid_multi.get() * 100.0),
+            9 => format!("{:.1}%", self.a_basic_multi.get() * 100.0),
+            10 => format!("{:.1}s", self.attack_duration.get()),
+            11 => format!("{:.1}s", self.release_duration.get()),
+            12 => format!("{:.1}s", self.distortion.get()),
+            _ => "".to_string(),
         }
     }
 
     fn get_parameter_name(&self, index: i32) -> String {
         match index {
-            0 => "Gain".to_string(),
-            1 => "Attack".to_string(),
-            2 => "Release".to_string(),
-            _ => "".to_string(),
+            0 => "White",
+            1 => "Perlin",
+            2 => "Value",
+            3 => "Worley",
+            4 => "RidgedMulti",
+            5 => "OpenSimplex",
+            6 => "Billow",
+            7 => "Cylinders",
+            8 => "HybridMulti",
+            9 => "BasicMulti",
+            10 => "Attack",
+            11 => "Release",
+            12 => "Distortion",
+            _ => "",
+        }.to_string()
+    }
+
+    fn get_parameter(&self, index: i32) -> f32 {
+        match index {
+            0 => self.a_white_noise.get(),
+            1 => self.a_perlin.get(),
+            2 => self.a_value.get(),
+            3 => self.a_worley.get(),
+            4 => self.a_ridged_multi.get(),
+            5 => self.a_open_simplex.get(),
+            6 => self.a_billow.get(),
+            7 => self.a_cylinders.get(),
+            8 => self.a_hybrid_multi.get(),
+            9 => self.a_basic_multi.get(),
+            10 => self.attack_duration.get(),
+            11 => self.release_duration.get(),
+            12 => self.distortion.get(),
+            _ => 0.0,
         }
     }
 
-    fn get_parameter_text(&self, index: i32) -> String {
+    fn set_parameter(&self, index: i32, val: f32) {
         match index {
-            0 => format!("{:.2}", self.gain.get()),
-            1 => format!("{:.2}", self.attack_duration.get()),
-            2 => format!("{:.2}", self.release_duration.get()),
-            _ => "".to_string(),
+            0 => self.a_white_noise.set(val),
+            1 => self.a_perlin.set(val),
+            2 => self.a_value.set(val),
+            3 => self.a_worley.set(val),
+            4 => self.a_ridged_multi.set(val),
+            5 => self.a_open_simplex.set(val),
+            6 => self.a_billow.set(val),
+            7 => self.a_cylinders.set(val),
+            8 => self.a_hybrid_multi.set(val),
+            9 => self.a_basic_multi.set(val),
+            10 => self.attack_duration.set(val.max(0.001)), // prevent division by zero
+            11 => self.release_duration.set(val.max(0.001)),
+            12 => self.distortion.set(val),
+            _ => (),
+        }
+    }
+}
+
+impl Default for KarplusParameters {
+    fn default() -> KarplusParameters {
+        KarplusParameters {
+            a_white_noise: AtomicFloat::new(1.0),
+            a_perlin: AtomicFloat::new(0.0),
+            a_value: AtomicFloat::new(0.0),
+            a_worley: AtomicFloat::new(0.0),
+            a_ridged_multi: AtomicFloat::new(0.0),
+            a_open_simplex: AtomicFloat::new(0.0),
+            a_billow: AtomicFloat::new(0.0),
+            a_cylinders: AtomicFloat::new(0.0),
+            a_hybrid_multi: AtomicFloat::new(0.0),
+            a_basic_multi: AtomicFloat::new(0.0),
+            attack_duration: AtomicFloat::new(0.5),
+            release_duration: AtomicFloat::new(0.5),
+            release_duration: AtomicFloat::new(1.0),
         }
     }
 }
 
 plugin_main!(Karplus);
-
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_karplus_new() {
-        let k = Karplus::new(440.0, 4400);
-        assert_eq!(k.frequency, 440.0);
-        assert_eq!(k.sample_rate, 4400);
-        assert_eq!(k.buffer.len(), 10);
-    }
-
-    #[test]
-    fn test_karplus_sample() {
-        let mut k = Karplus::new(440.0, 44100);
-        k.buffer = vec![1.0, 0.0];
-        let sample = k.sample(1.0);
-        assert_eq!(sample, 0.5)
-    }
-}
-*/
